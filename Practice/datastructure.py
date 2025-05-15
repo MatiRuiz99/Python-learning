aaf910d71db580a216db3047c99b3b6bcf9118a0
Matias Ruiz <matiasruiz94m@gmail.com>
2025-05-15 19:00:48 -0300

    data structures

---
+++ Practice/datastructure.py
@@ -0,0 +1,36 @@
+#List
+#General purpose, can be re-sized, sequence type, sortable.
+#Uses [] 
+#delete, .append, .extend, .insert, .pop, .remove, .reverse, .sort
+
+#Tuple
+#Immutable, fixed data, Faster than list, sequence type
+#Uses () or variables separated by , and nothing else
+
+#Set
+#Can't store duplicates, faster than list, math set ops, unordered
+#Uses {}, when created removes dupes
+# .add, .remove, .clear
+# AND &, OR |, XOR ^, DIFF -, SUBSET <=, SUPERSET >=
+
+#Dict
+#Key/value pairs structure, associative array, unordered
+#uSES {"KEY": VALUE, "KEY": VALUE}
+#Add or change x["key"] = value, del, len, in/not in, .clear
+#.keys, .values, .items
+#iterating with for
+
+#Sequence Type
+#A sequence data structure can:
+#Store ordered elements
+#Support indexing (positive/negative)
+#Support slicing
+#Iterate through elements
+#Check membership (in, not in)
+#Get length (len())
+#Concatenate (+)
+#Repeat (*)
+#Compare (==, <, etc.)
+#Count elements (.count())
+#Find index of element (.index())
+#Unpacking
